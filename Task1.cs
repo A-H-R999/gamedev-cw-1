efc5222ef2df35285cf82b898059f129e6953ec9
Ali Ramadan <aliramadan99kw@gmail.com>
2020-12-20 19:24:45 +0300

    Create Task1.cs
    1st push for task1

---
+++ Task1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String characterName = "Ryuk";
+            double speed = 270.5;
+            int age = 872;
+            String superPower1 = "Invisibility";
+            String superPower2 = "Flying";
+
+            Console.WriteLine("The character is " + characterName + ", his speed is " + speed + " and he is " + age + " years old.");
+            Console.WriteLine("He have 2 super powers, the 1st one is " + superPower1 + ", and the 2nd super power is " + superPower2 + ".");
+        }
+    }
+}
